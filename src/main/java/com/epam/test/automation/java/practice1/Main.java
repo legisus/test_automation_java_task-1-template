6d29a601cf2ea88c11582a636e58999f38f3d9bb
Mykola <legisus@gmail.com>
2021-07-08 20:33:11 +0200

    Initial commit

--- src/main/java/com/epam/test/automation/java/practice1/Main.java
+++ src/main/java/com/epam/test/automation/java/practice1/Main.java
@@ -1,15 +1,20 @@
 package com.epam.test.automation.java.practice1;
 
 public class Main {
-
     /**
      * <summary>
      * Implement code according to description of task 1.
      * </summary>
      */
     public static int task1(int n) {
-        //TODO: Delete line below and write your own solution;
-        throw new UnsupportedOperationException();
+        if (n > 0) {
+            n = n * n;
+        } else if (n < 0) {
+            n = Math.abs(n);
+        } else {
+            n = 0;
+        }
+        return n;
     }
 
     /**
@@ -18,8 +23,44 @@ public class Main {
      * </summary>
      */
     public static int task2(int n) {
-        //TODO: Delete line below and write your own solution;
-        throw new UnsupportedOperationException();
-    }
+        int array[] = new int[3];
+        String number = String.valueOf(n);
+        for (int i = 0; i < number.length(); i++) {
+            int j = Character.digit(number.charAt(i), 10);
+            for (int a = 0; a < array.length; i++) {
+                array[a] = array[j];
+            }
+        }
+        int a = array[0];
+        int b = array[1];
+        int c = array[2];
+        int result = 0;
 
+
+        if (a > b && a > c && b > c) {
+            result = Integer.parseInt(a + "" + b + "" + c);
+        } else {
+            if (a >= b && a >= c && b <= c) {
+                result = Integer.parseInt(a + "" + c + "" + b);
+            } else {
+                if (a <= b && a >= c && b >= c) {
+                    result = Integer.parseInt(b + "" + a + "" + c);
+                } else {
+                }
+                if (a <= b && a <= c && b >= c) {
+                    result = Integer.parseInt(b + "" + c + "" + a);
+                } else {
+                    if (a >= b && a <= c && b <= c) {
+                        result = Integer.parseInt(c + "" + a + "" + b);
+                    } else {
+                        if (a <= b && a <= c && b <= c) {
+                            result = Integer.parseInt(c + "" + b + "" + a);
+                        } else {
+                        }
+                    }
+                }
+            }
+        }
+        return result;
+    }
 }
