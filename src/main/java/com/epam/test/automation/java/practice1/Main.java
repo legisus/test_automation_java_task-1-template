735185209f70f3ee2b8281edd48d96e29800017a
Mykola <legisus@gmail.com>
2021-07-11 23:00:44 +0200

    last one

--- src/main/java/com/epam/test/automation/java/practice1/Main.java
+++ src/main/java/com/epam/test/automation/java/practice1/Main.java
@@ -1,5 +1,10 @@
 package com.epam.test.automation.java.practice1;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
 public class Main {
     /**
      * <summary>
@@ -23,44 +28,11 @@ public class Main {
      * </summary>
      */
     public static int task2(int n) {
-        int array[] = new int[3];
-        String number = String.valueOf(n);
-        for (int i = 0; i < number.length(); i++) {
-            int j = Character.digit(number.charAt(i), 10);
-            for (int a = 0; a < array.length; i++) {
-                array[a] = array[j];
-            }
-        }
-        int a = array[0];
-        int b = array[1];
-        int c = array[2];
-        int result = 0;
+        List<String> putInString = Arrays.asList(String.valueOf(n).split(""));
+        putInString.sort(Comparator.reverseOrder());
 
-
-        if (a > b && a > c && b > c) {
-            result = Integer.parseInt(a + "" + b + "" + c);
-        } else {
-            if (a >= b && a >= c && b <= c) {
-                result = Integer.parseInt(a + "" + c + "" + b);
-            } else {
-                if (a <= b && a >= c && b >= c) {
-                    result = Integer.parseInt(b + "" + a + "" + c);
-                } else {
-                }
-                if (a <= b && a <= c && b >= c) {
-                    result = Integer.parseInt(b + "" + c + "" + a);
-                } else {
-                    if (a >= b && a <= c && b <= c) {
-                        result = Integer.parseInt(c + "" + a + "" + b);
-                    } else {
-                        if (a <= b && a <= c && b <= c) {
-                            result = Integer.parseInt(c + "" + b + "" + a);
-                        } else {
-                        }
-                    }
-                }
-            }
-        }
-        return result;
+        StringBuilder stringBuilder = new StringBuilder();
+        putInString.forEach(stringBuilder::append);
+        return Integer.parseInt(stringBuilder.toString());
     }
 }
